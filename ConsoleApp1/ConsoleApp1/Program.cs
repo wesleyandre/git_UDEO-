a2ecf2d2a7ad5e4a7a4d6790fa2b5267c0dda17a
Misafuchi <masichuchi1@gmail.com>
2026-09-03 10:43:56 -0600

    feat/:(Ingresa mi apellido)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Misas");
+            Console.WriteLine("Misas Suchi");
         }
     }
 }
